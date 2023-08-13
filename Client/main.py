0c2316933e9c3f980bd146f531f68ee894e021be
PC_HOME <1134837914@qq.com>
2023-08-13 19:27:47 +0800

    QT主窗口

--- Client/main.py
+++ Client/main.py
@@ -1,10 +1,14 @@
 import sys
+import os
 
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from qt_material import apply_stylesheet
+from UI.MainWindow import*
+from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow
+import UI.ButtonResource_rc
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print(1)
+    app = QApplication(sys.argv)
+    MainWindow =  PyQt5.QtWidgets.QMainWindow()
+    ui = MainWindow()
+    ui.setupUI(MainWindow)
+    sys.exit(app.exec_)
